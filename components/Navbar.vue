4080c450fbb5b86f2fd034d40959ba5e133ebd15
Yury Sokolov <r4v7n@yandex.ru>
2021-03-05 00:14:13 +0500

    Структура проекта

---
+++ components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+	<header class="header">
+            <div class="container">
+                <div class="header__hamburger hamburger">
+                    <div class="hamburger__item hamburger__item-one"></div>
+                    <div class="hamburger__item hamburger__item-two"></div>
+                    <div class="hamburger__item hamburger__item-three"></div>
+                </div>
+                <div class="header__profile profile">
+                    <div class="profile__status">
+                        <img src="img/icon.svg" alt="icon">
+                        <span>Мегамодник</span>
+                    </div>
+                    <div class="profile__bonuses btn">200</div>
+                    <div class="profile__avatar">
+                        <img src="img/avatar.png" alt="icon">
+                    </div>
+                </div>
+            </div>
+        </header>
+</template>
